2438fb00cc12e343a4950e8a081b9dc213f45bff
adriana <picadoadriana7@gmail.com>
2021-01-24 19:42:02 -0500

    commit tree 9b3d0a1bfda3f8d263595d815d56dc9e976a74e1

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Adriana"
+      end 
+
+
+      get '/hometown' do
+        "My hometown is Summit, NJ"
+      end 
+
+
+      get '/favorite-song' do
+        "My favorite song is '505'"
+      end 
+
 end
